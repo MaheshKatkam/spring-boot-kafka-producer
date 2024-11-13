cf4bbd3c9657ab70060576ffcc97f5f668f7754c
Mahesh Katkam <mahesh.katkam@macys.com>
2024-11-13 11:03:13 +0530

    First commit for kafka producer changes.

---
+++ src/test/java/com/springboot/kafka/producer/Kafka_Producer_Demo/KafkaProducerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka.producer.Kafka_Producer_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
